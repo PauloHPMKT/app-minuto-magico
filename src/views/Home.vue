9d779e397405d79054dc3f5e11d7c7314ebafead
Paulo Sergio <pauloserg861@gmail.com>
2024-12-21 15:40:51 -0300

    chore: add project structure

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <p>Click on the links above to navigate.</p>
+  </div>
+</template>
