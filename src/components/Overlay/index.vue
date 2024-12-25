4e27f49cb113263ff32d73fe9fca697004d1c12b
Paulo Sergio <pauloserg861@gmail.com>
2024-12-25 15:29:13 -0300

    create ovelay

---
+++ src/components/Overlay/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="overlay">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.overlay {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  height: 100%;
+  z-index: 300;
+  background-color: rgba(18, 18, 18, 0.6);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
