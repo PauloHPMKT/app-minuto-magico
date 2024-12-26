b608c16b872433a17400c82c03fadd9d58a0cee8
Paulo Sergio <pauloserg861@gmail.com>
2024-12-26 17:04:05 -0300

    refactor: apply env to hidden api connection

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import App from './App.vue'
 
 import { router } from './router'
 import { AddChildService } from './services/trampoline/add-child/add-child.service'
-import { ApiConnection } from './services/http/api'
+import { ApiConnection } from './services/infra/http/api'
 import { GetChildrenService } from './services/trampoline/get-children/get-children.service'
 
 const app = createApp(App)
 
-const apiConnection = new ApiConnection('http://localhost:3003/api/children')
+const apiConnection = new ApiConnection(import.meta.env.VITE_API_URL)
 const addChildService = new AddChildService(apiConnection)
 const getChildrenService = new GetChildrenService(apiConnection)
 app.provide('addChildService', addChildService)
